d861718ed28b8f7488397abc34b075cf12312cf9
Jerome Spooner <jspooner@brooklynsteamcenter.org>
2023-05-12 14:55:03 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue';
+let todos = ref(['A','B','C'])
+
+</script>
+
+<template>
+  <h1>Todo Application</h1>
+  <p>{{ todos }}</p>
+  <p v-for="todo in todos">
+        {{todo}}
+</p>
+<input>
+<button>Add Todo</button>
+
+</template>
+ 
+<style >
+</style>
