94871ef4a9d2e15970b535a1f84976aa3ca1a331
Jerome Spooner <jspooner@brooklynsteamcenter.org>
2023-05-15 14:54:02 -0400

    i caught up to date

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
 <script setup>
-import { ref } from 'vue';
-let todos = ref(['A','B','C'])
+import { ref, watch } from 'vue';
+let todos = ref(JSON.parse(window.localStorage.getItem('todos')))
+let newTodo = ref('')
+function addTodo () {
+todos.value.push({text: newTodo.value,complete: false})
+complete: false
+	newTodo.value = ''
+
+}
+watch(todos, function(value) {
+      window.localStorage.setItem('todos', JSON.stringify(value))
+    }, {deep: true})
 
 </script>
 
 <template>
   <h1>Todo Application</h1>
-  <p>{{ todos }}</p>
-  <p v-for="todo in todos">
-        {{todo}}
-</p>
-<input>
-<button>Add Todo</button>
+  <li v-for="todo in todos">
+    <input type="checkbox" v-model =  "todo.complete">
+
+{{todo.text}}
+<button @click="delete">Delete</button>
+  </li>
+
+  <input v-model="newTodo">
+  <button @click="addTodo">Add Todo</button>
+
+
 
 </template>
  
